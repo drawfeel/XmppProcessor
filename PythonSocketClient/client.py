8cb63da32f8e41628ff1aa6f98bb838731c70af7
drawfeel <82626662+drawfeel@users.noreply.github.com>
2021-08-06 10:25:17 +0800

    Add files via upload
    This is the test client codes which written with python

---
+++ PythonSocketClient/client.py
@@ -0,0 +1,34 @@
+import socket
+
+class Client():
+    def send_msg(self):
+        LOCAL_PORT=7667
+        goon=True
+        try:
+            socket.setdefaulttimeout(5)
+            s=socket.socket()
+            s.connect(("localhost", LOCAL_PORT))
+            print("input data ('EXIT' for shutting server down)")
+            while(goon):
+                msg = input(">>>").strip()
+                if len(msg)==0 :
+                    continue
+                s.send(msg.encode("utf-8"))
+                s.send("\r\n".encode("utf-8"))
+
+                s.send(msg.encode("utf-8"))
+                s.send("\r\n".encode("utf-8"))
+
+#                data=s.recv(256)
+#                if len(data) > 0 :
+#                    print("recv:>", data.decode("utf-8"))
+                if "EXIT" == msg:
+                    goon=False;
+                    print("BYE ...")
+                    s.close()
+        except ConnectionError as ex:
+            print(ex)
+
+if __name__ == "__main__":
+    client=Client()
+    client.send_msg()
